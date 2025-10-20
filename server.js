82ae82a79d4f5aae8fb4f2c3c85c5fbbb6d6f87c
Christopher Crow <0chriscrew0@gmail.com>
2025-10-20 15:24:02 -0500

    configure base GET route

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8383;
 
+app.get("/", (req, res) => {
+  console.log("I hit and endpoint!", req.method);
+  res.sendStatus(200);
+});
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

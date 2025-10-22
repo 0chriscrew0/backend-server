ff1dcc2aaf8269fb326b07c0e164694d1c4b03cb
Christopher Crow <0chriscrew0@gmail.com>
2025-10-22 11:52:55 -0500

    configure api route

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 8383;
 
+const data = {
+  name: "chris",
+};
+
 app.get("/", (req, res) => {
   res.send("<h1>Home</h1>");
 });
 
 app.get("/dashboard", (req, res) => {
   res.send("<h1>Dashboard</h1>");
+});
+
+app.get("/api/data", (req, res) => {
+  res.send(data);
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
